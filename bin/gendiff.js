90ba6a540ba9d3e80950a0994c7d65283c087975
Admin1 <altbio22@yandex.ru>
2023-04-02 19:51:28 +0300

    test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,4 @@ import { program } from 'commander';
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1'), '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
+    .option('-f, --format <type>', 'output format');
