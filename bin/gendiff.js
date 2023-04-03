ea1bffa5014c68f8283b550d60a4375096eb2e93
Admin1 <altbio22@yandex.ru>
2023-04-03 18:50:03 +0300

    test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,13 @@
 
 import { program } from 'commander';
 
-program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1'), '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format'); 1
+const program = new Command();
+
+const command = () => {
+    console.log('Hello, World!');
+  };
+  
+  program
+    .version('0.0.1')
+    .action(command)
+    .parse(process.argv);
