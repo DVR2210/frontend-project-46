2eb6c19d2db458cf310524f4e9f10fc0b4caab72
Admin1 <altbio22@yandex.ru>
2023-04-04 19:47:48 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { Command } from 'commander';
 
 const program = new Command();
 
-const command = () => {
-    console.log('Hello, World!');
-  };
-  
-  program
-    .version('0.0.1')
-    .action(command)
-    .parse(process.argv);
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+ 
+program.parse();
