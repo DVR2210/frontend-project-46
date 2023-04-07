2cbd52be4a1e893456d0b08586bf60b422a5b096
Admin1 <altbio22@yandex.ru>
2023-04-07 15:24:07 +0300

    step3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+
 import readFileSync from 'fs';
+import _ from 'lodash';
 
-const data = fs.readFileSync(path, 'utf-8'); // функция получает путь к фалам 
+const gendiff = (obj1, obj2) => {
+    const result = {};
+    const keys1 = _.keys(obj1);
+    const keys2 = _.keys(obj2);
+    const keys = _.union(keys1, keys2);
+    
+    for (let key of keys) {
+      if (!_.has(obj1, key)) {
+      result[key] = 'added';
+      
+    } else if (!_.has(obj2, key)) {
+      result[key] = 'deleted';
+      
+    } else if (obj1[key] !== obj2[key]) {
+      result[key] = 'changed';
+      
+    } else {
+      result[key] = 'unchanged';
+    }
+  }
+    return result;
+    
+  };
 
-const dataPars = JSON.parse(data); // тут получаем объект 
 
 export default gendiff;
+
+// const data1 = fs.readFileSync(path, 'utf-8'); // функция получает путь к фалам 
+
+// const dataPars = JSON.parse(data); // тут получаем объект 
