9d31e32e86f5c4cab5f3fa551b001a8f6331f30f
Admin1 <altbio22@yandex.ru>
2023-04-13 18:37:13 +0300

    step4

---
+++ __test__/index.test.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@jest/globals';
+import { readFileSync } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+
+test('file json', () => {
+    const filename1 = getFixturePath('file1.json');
+    const filename2 = getFixturePath('file2.json');
+    const resultName = getFixturePath('file_result.txt');
+    const result = readFileSync(resultName, 'utf8');
+    expect(parser(filename1, filename2)).toBe(result);
+});
