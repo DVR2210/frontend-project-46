76b028ed290e3cd4e53afd2764f1ebb4af76afdd
Admin1 <altbio22@yandex.ru>
2023-04-17 17:31:53 +0300

    step4

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,12 +1,13 @@
-import { test, expect } from '@jest/globals';
-import { readFileSync } from 'fs';
-import path { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import gendiff from '../src/index.js';
+import { test } from '@jest/globals'; // импорт джеста 
+import { readFileSync } from 'fs'; // чтение файла 
+import { path } from 'path'; // модуль чтения путей 
+import { fileURLToPath } from 'url'; // функция декодирования элемнетов 
+import gendiff from '../src/index.js'; // функция итогавая сборка
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url); // поулчаем путь до файла - абсолютный путь? 
+const __dirname = path.dirname(__filename); // получаем название папки в которйо находимся 
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename); // метод джоин генирирует путь 
 
 
 test('file json', () => {
@@ -14,5 +15,5 @@ test('file json', () => {
     const filename2 = getFixturePath('file2.json');
     const resultName = getFixturePath('file_result.txt');
     const result = readFileSync(resultName, 'utf8');
-    expect(gendiff (filename1, filename2)).toBe(result);
+    expect(gendiff(filename1, filename2)).toBe(result);
 });
