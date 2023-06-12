43c6cce49a7c7048fb51fb79aa36eefc2e26ac5a
Admin1 <altbio22@yandex.ru>
2023-06-12 11:58:00 +0300

    node --experemental

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,43 +1,212 @@
-import { test, expect } from '@jest/globals'; 
-import { readFileSync } from 'fs'; 
-import path from 'path'; 
-import { fileURLToPath } from 'url'; 
-import genDiff from '../src/index.js'; 
+// import { test, expect } from '@jest/globals'; 
+// import { readFileSync } from 'fs'; 
+// import path from 'path'; 
+// import { fileURLToPath } from 'url'; 
+// import genDiff from '../src/index.js'; 
 
-const getFixturePath = (file) => {
-    const __filename = fileURLToPath(import.meta.url); 
-    const __dirname = path.dirname(__filename); 
-    return path.join(__dirname, '..', '__fixtures__', file); 
-};
+// const getFixturePath = (file) => {
+//     const __filename = fileURLToPath(import.meta.url); 
+//     const __dirname = path.dirname(__filename); 
+//     return path.join(__dirname, '..', '__fixtures__', file); 
+// };
 
-test('file json', () => {
-   const filename1 = getFixturePath('file1.json');
-   const filename2 = getFixturePath('file2.json');
-    const resultName = getFixturePath('file_result.txt');
-    const result = readFileSync(resultName, 'utf8');
-    expect(genDiff(filename1, filename2)).toEqual(result);
-});
+// test('file json', () => {
+//    const filename1 = getFixturePath('file1.json');
+//    const filename2 = getFixturePath('file2.json');
+//     const resultName = getFixturePath('file_result.txt');
+//     const result = readFileSync(resultName, 'utf8');
+//     expect(genDiff(filename1, filename2)).toEqual(result);
+// });
 
-test('file yaml', () => {
-    const filename1 = getFixturePath('file1.yaml');
-    const filename2 = getFixturePath('file2.yaml');
-    const resultName = getFixturePath('file_result.txt');
-    const result = readFileSync(resultName, 'utf8');
-    expect(genDiff(filename1, filename2)).toEqual(result);
-});
+// test('file yaml', () => {
+//     const filename1 = getFixturePath('file1.yaml');
+//     const filename2 = getFixturePath('file2.yaml');
+//     const resultName = getFixturePath('file_result.txt');
+//     const result = readFileSync(resultName, 'utf8');
+//     expect(genDiff(filename1, filename2)).toEqual(result);
+// });
 
-test('file yml', () => {
-    const filename1 = getFixturePath('file1.yml');
-    const filename2 = getFixturePath('file2.yml');
-    const resultName = getFixturePath('file_result.txt');
-    const result = readFileSync(resultName, 'utf8');
-    expect(genDiff(filename1, filename2)).toEqual(result);
-});
+// test('file yml', () => {
+//     const filename1 = getFixturePath('file1.yml');
+//     const filename2 = getFixturePath('file2.yml');
+//     const resultName = getFixturePath('file_result.txt');
+//     const result = readFileSync(resultName, 'utf8');
+//     expect(genDiff(filename1, filename2)).toEqual(result);
+// });
 
-test('plain', () => {
-    const filename1 = getFixturePath('file1.yml');
-    const filename2 = getFixturePath('file2.yml');
-    const resultName = getFixturePath('file_result_plain.txt');
-    const result = readFileSync(resultName, 'utf8');
-    expect(genDiff(filename1, filename2, 'plain')).toEqual(result);
+// test('plain', () => {
+//     const filename1 = getFixturePath('file1.yml');
+//     const filename2 = getFixturePath('file2.yml');
+//     const resultName = getFixturePath('file_result_plain.txt');
+//     const result = readFileSync(resultName, 'utf8');
+//     expect(genDiff(filename1, filename2, 'plain')).toEqual(result);
+//   });
+
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import genDiff from '../src/index.js';
+
+describe('gendiff', () => {
+  const getFixturePath = (filename) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    return path.join(__dirname, '..', '__fixtures__', filename);
+  };
+  const jsonPath1 = getFixturePath('file1.json');
+  const jsonPath2 = getFixturePath('file2.json');
+  const ymlPath1 = getFixturePath('file1.yml');
+  const ymlPath2 = getFixturePath('file2.yml');
+  const yamlPath1 = getFixturePath('file1.yaml');
+  const yamlPath2 = getFixturePath('file2.yaml');
+  const mdPath1 = getFixturePath('file1.md');
+  const mdPath2 = getFixturePath('file2.md');
+  test('check output difference stylish with all formats', () => {
+    expect(genDiff(jsonPath1, jsonPath2)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+    expect(genDiff(yamlPath1, yamlPath2)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+    expect(genDiff(ymlPath1, ymlPath2)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+    expect(() => genDiff(mdPath1, mdPath2)).toThrow();
   });
+  test('formatter check', () => {
+    expect(genDiff(jsonPath1, yamlPath2, 'plain')).toEqual(`Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`);
+    expect(() => genDiff(ymlPath1, ymlPath2, 'md')).toThrow();
+  });
+});
