5cd2845818bd753616a079dbef7d6c30ceb7b68b
Admin1 <altbio22@yandex.ru>
2023-06-19 10:39:28 +0300

    step8 tes none

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,14 +1,22 @@
 import _ from 'lodash';
 
-const genIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat((depth * spacesCount) - 2);
+const space = ' ';
+const spaceCount = 4;
 
-const stringify = (nodeValue, depth = 1) => {
-  if (!(_.isObject(nodeValue))) {
-    return `${nodeValue}`;
+const genIndent = (depth) => space.repeat(depth * spaceCount).slice(0, -2);
+
+
+const stringify = (value, depth) => {
+  if (!_.isPlainObject(value)) {
+    return String(value);
   }
-  const result = Object.entries(nodeValue).map(([key, value]) => `${genIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
-  return `{\n${result.join('\n')}\n  ${genIndent(depth)}}`;
+  const lines = Object
+    .entries(value)
+    .map(([key, val]) => `${genIndent(depth + 1)}  ${key}: ${stringify(val, (depth + 1))}`);
+  return `{\n${lines.join('\n')}\n${genIndent(depth)}  }`;
 };
+
+
 const stylish = (tree) => {
   const iter = (node, depth = 1) => {
     const result = node.map((item) => {
@@ -20,7 +28,7 @@ const stylish = (tree) => {
         case 'deleted':
           return `${genIndent(depth)}- ${item.name}: ${stringify(item.value, depth)}`;
         case 'unchanged':
-          return `${genIndent(depth)}  ${item.name}: ${stringify(item.value, depth)}`;
+          return `${genIndent(depth)} ${item.name}: ${stringify(item.value, depth)}`;
         case 'changed':
           return `${genIndent(depth)}- ${item.name}: ${stringify(item.value1, depth)}\n${genIndent(depth)}+ ${item.name}: ${stringify(item.value2, depth)}`;
         default:
